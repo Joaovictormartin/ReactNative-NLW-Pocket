c3e9e172997b71c9b61880aee11f2a6b903a1d83
Joao Victor <devjoaovictor10@gmail.com>
2024-12-28 19:24:10 -0300

    refactor: ajuste no loading

--- mobile/src/components/Loading/index.tsx
+++ mobile/src/components/Loading/index.tsx
@@ -1,5 +1,7 @@
-import { colors } from "@/styles/colors";
 import { ActivityIndicator } from "react-native";
+
+import { colors } from "@/styles/colors";
+
 import { styles } from "./styles";
 
 export function Loading() {
